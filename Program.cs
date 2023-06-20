2710423b8ca3f9c6fd55e7364addff6b6d54f014
error9098x <kainturarenu@gmail.com>
2023-06-20 16:08:38 +0530

    added code

--- Program.cs
+++ Program.cs
@@ -184,6 +184,7 @@ namespace MyNamespace {
         //input the range and find all the prime numbers in the range
         Console.WriteLine("Enter the range");
         int range = Convert.ToInt32(Console.ReadLine());
+        int counter = 0;
         for (int i =2;i<=range;i++){
             for (int j = 2; j <= i; j++){
                 if (i % j == 0){
@@ -191,9 +192,11 @@ namespace MyNamespace {
                 }
                 else if (j == i - 1){
                     Console.Write(i+" ");
+                    counter++;
                 }
             }
         }
+        Console.WriteLine("\nThere are "+counter+" prime numbers in the range");
     }
 }
 }
