ef13299715aee678b8324423061d813fcbb57779
error9098x <kainturarenu@gmail.com>
2023-06-20 15:34:30 +0530

    dotnet

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyNamespace {
+    class ChooseProgram{
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!1");
+            MyMethod();
+        }
+
+    static void MyMethod(){
+     Console.WriteLine("Hello, World!2");
+    }
+}
+}
