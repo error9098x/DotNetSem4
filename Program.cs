8ccc78dd5bbce9d56abbaef4a84838b4831bd36b
error9098x <kainturarenu@gmail.com>
2023-06-21 12:22:13 +0530

    added code new

--- Program.cs
+++ Program.cs
@@ -282,5 +282,23 @@ static void swap(){
     Console.WriteLine("After Swap x is " + x);
     Console.WriteLine("After Swap y is " + y);
 }
+
+static void smallest(){
+    Console.WriteLine("Enter the size of array");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[size];
+    for (int i = 0 ; i < size ; i++){
+        Console.WriteLine("Enter the element");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    int smallest = arr[0];
+    for (int i = 0;i < size ; i++){
+        if (arr[i] < smallest){
+            smallest = arr[i];
+        }
+    }
+    Console.WriteLine("The smallest number is " + smallest);
+}
+
 }
 }
